e21a15ea4b3a5638601b379952ee2088dfe19b0b
CarlosHenrique405 <miranda.arames@gmail.com>
2019-11-05 08:50:56 -0200

    Soma de Matriz

---
+++ Kahoot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Kahoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = {{10,15,48},{56,97,77},{2,100,33}};
+
+            int soma = 0;
+            for(int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for(int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    soma += matriz[i,j];
+                }
+            }
+
+            // foreach(var item in matriz)
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+            Console.WriteLine(soma);
+
+
+        }
+    }
+}
