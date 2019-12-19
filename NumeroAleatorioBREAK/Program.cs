40fcc37791bf1a4e018a9620c27a38630613f7fe
Carlos-Henrique98 <ichfs9108@gmail.com>
2019-12-19 14:48:18 -0300

    BREAK

---
+++ NumeroAleatorioBREAK/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NumeroAleatorioBREAK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numero = random.Next(1,51);
+
+            Console.WriteLine("O número escolhido foi: {0}",numero);
+            Console.WriteLine();
+
+            for(int i = 0; i < 50; i++)
+            {
+                Console.WriteLine("O número escolhido é:{0}",i);
+                if(numero == i)
+                {
+                    Console.WriteLine("Sim, esse foi o número escolhido!!!");
+                    break;
+                }
+                else{
+                    Console.WriteLine("Não, esse não foi o número escolhido!!!");
+                }
+            }
+        }
+    }
+}
