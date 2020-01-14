dffcc9bc48d741d33525d5278d1d266f7c841c8d
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-01-13 23:56:59 -0300

    Revisao

---
+++ RevisaoExer/SenhaValida/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SenhaValida
+{
+    class Program
+    {
+        //Escreva um programa que repita a leitura de uma senha até que
+        //ela seja válida. Para cada leitura de senha incorreta informada,escrever
+        //a mensagem "Senha Invalida". Quando a senha for informada corretamente deve
+        //ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere
+        // que a senha correta é o valor 2002.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===================");
+            Console.WriteLine("       SENHA !");
+            Console.WriteLine("===================");
+
+            Console.Write("Insira a sua senha: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while(senha != 2002)
+            {
+                Console.WriteLine("Senha Incorreta!");
+                Console.Write("Digite novamente a senha: ");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Senha Correta!");
+        }
+    }
+}
---
+++ RevisaoExer/SistemaCartesiano/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SistemaCartesiano {
+    class Program {
+        static void Main (string[] args) {
+            Console.Write("Digite as coordenadas: ");
+            string[] cartesiano = Console.ReadLine ().Split (' ');
+            double eixoX = double.Parse (cartesiano[0]);
+            double eixoY = double.Parse (cartesiano[1]);
+
+            while (eixoX != 0 && eixoY != 0) {
+                if (eixoX > 0 && eixoY > 0) {
+                    Console.WriteLine ("Primeiro Quadrante!");
+                } else if (eixoX > 0 && eixoY < 0) {
+                    Console.WriteLine ("Quarto Quadrante!");
+                } else if (eixoX < 0 && eixoY < 0) {
+                    Console.WriteLine ("Terceiro Quadrante!");
+                } else if (eixoX < 0 && eixoY > 0) {
+                    Console.WriteLine ("Segundo Quadrante!");
+                }
+                Console.WriteLine();
+                Console.Write("Digite Novamente: ");
+            cartesiano = Console.ReadLine().Split(' ');
+            eixoX = double.Parse(cartesiano[0]);
+            eixoY = double.Parse(cartesiano[1]);
+            }
+        }
+    }
+}
