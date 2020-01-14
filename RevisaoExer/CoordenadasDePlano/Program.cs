93b7e5c5ee29f1157920712097ecf12324a553d5
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-01-13 22:43:25 -0300

    Revisao

---
+++ RevisaoExer/CoordenadasDePlano/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CoordenadasDePlano
+{
+    class Program
+    {
+        //Leia 2 valores com uma casa decimal (x e y), que devem 
+        //representar as coordenadas de um ponto em um plano A seguir,
+        //determine qual o quadrante ao qual pertence o ponto, ou se 
+        //esta sobre um dos eixos cartesianos ou na origem (x=y=0).
+        //Se o ponto estiver na origem, escreva a mensagem "Origem".
+        //Se o ponto estiver sobre um dos eixos escreva "Eixo X" ou 
+        //Eixo Y", conforme for a situação.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[ QUADRANTE ]");
+            Console.WriteLine();
+            Console.Write("Informe os valores: ");
+            string [] vetor = Console.ReadLine().Split(' ');
+            double eixoX = double.Parse(vetor[0]);
+            double eixoY = double.Parse(vetor[1]);
+
+            if(eixoX > 0 && eixoY > 0)
+            {
+                Console.WriteLine("[ Q1 ]");
+            }
+            else if(eixoX < 0 && eixoY > 0)
+            {
+                Console.WriteLine(" [ Q2 ]");
+            }
+            else if(eixoX < 0 && eixoY < 0)
+            {
+                Console.WriteLine("[ Q3 ]");
+            }
+            else if(eixoX > 0 && eixoY < 0)
+            {
+                Console.WriteLine("[ Q4 ]");
+            }
+            else if(eixoX == 0 && eixoY == 0)
+            {
+                Console.WriteLine("Origem!!!");
+            }
+            else if(eixoX == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if(eixoY == 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+
+
+        }
+    }
+}
---
+++ RevisaoExer/PaisImaginario/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PaisImaginario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double imposto = 0;
+
+            Console.WriteLine("[ IMPOSTO DE RENDA ]");
+            Console.WriteLine();
+            Console.Write("Informe o seu salario: R$ ");
+            double salario = double.Parse(Console.ReadLine());
+
+            if(salario <= 2000.00)
+            {
+                Console.WriteLine("ISENTO!!!");
+                Console.WriteLine("Imposto: R$ 0");
+            }
+            else if(salario <= 3000.00)
+            {
+                imposto = (salario - 2000.00) *0.08;
+                Console.WriteLine("Imposto: R${0}",imposto);
+            }
+            else if(salario <= 4500.00)
+            {
+                imposto = (salario - 3000.00) * 0.18 + 1000 *0.08;
+                Console.WriteLine("Imposto: R$ {0}",imposto);
+            }
+            else
+            {
+                imposto = (salario - 4500.00) *0.28 + 1500.00 * 0.18 + 1000 * 0.08;
+                Console.WriteLine("Imposto: R${0}",imposto);
+            }
+   
+        }
+    }
+}
