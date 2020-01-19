c7c218adec566e08526c24188aa8b3614ee41e78
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-01-19 02:05:07 -0300

    Revisao

---
+++ RevisaoExer/MatrizNumeroNegativo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MatrizNumeroNegativo {
+    class Program {
+        //Fazer um programa para ler um número inteiro N e uma Matriz
+        //de ordem N contendo números inteiros. Em seguida, mostrar a 
+        //diagonal principal e a quantidade de valores negativos da matriz
+
+        static void Main (string[] args) {
+            Console.WriteLine ("[ DIAGONAL ]");
+            Console.WriteLine ();
+
+            Console.Write ("Quantas Linhas e Colunas: ");
+            int linhasColunas = int.Parse (Console.ReadLine ());
+
+            int [,]
+
+            for (int i = 0; i < linhasColunas; i++) 
+            {
+                string[] vetor = Console.ReadLine ().Split (' ');
+                for (int j = 0; j < linhasColunas; j++) 
+                {
+                    vetor[i,j] = int.Parse()
+                }
+            }
+
+        }
+    }
+}
