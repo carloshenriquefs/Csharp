5d2402a01e4c8fc86f40240b4a49e6453bb8526c
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-01-19 15:31:56 -0300

    Revisao M

--- RevisaoExer/MatrizNumeroNegativo/Program.cs
+++ RevisaoExer/MatrizNumeroNegativo/Program.cs
@@ -7,22 +7,44 @@ namespace MatrizNumeroNegativo {
         //diagonal principal e a quantidade de valores negativos da matriz
 
         static void Main (string[] args) {
-            Console.WriteLine ("[ DIAGONAL ]");
+            Console.WriteLine ();
+            Console.WriteLine ("      [ DIAGONAL ]");
             Console.WriteLine ();
 
             Console.Write ("Quantas Linhas e Colunas: ");
             int linhasColunas = int.Parse (Console.ReadLine ());
 
-            int [,]
+            int [,] math = new int[linhasColunas, linhasColunas];
 
             for (int i = 0; i < linhasColunas; i++) 
             {
                 string[] vetor = Console.ReadLine ().Split (' ');
                 for (int j = 0; j < linhasColunas; j++) 
                 {
-                    vetor[i,j] = int.Parse()
+                    math[i,j] = int.Parse(vetor[j]);
                 }
             }
+
+            Console.WriteLine();
+            Console.Write("Matriz Diagonal: ");
+            for(int i = 0; i < linhasColunas; i++)
+            {
+                Console.Write(math[i,i] + "");
+            }
+
+            int contador = 0;
+            for(int i = 0; i < linhasColunas; i++)
+            {
+                for(int j = 0; j < linhasColunas; j++)
+                {
+                    if(math[i,j] < 0)
+                    {
+                        contador++;
+                    }
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Números negativos: "+contador);
 
         }
     }
