98ceea1faa3d61c5bd76a2e7427f97f6c977f847
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-09 15:06:54 -0300

    Revisao

---
+++ RevisaoExer/DiferencaForDiferenca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiferencaForDiferenca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RevisaoExer/DiferencaValores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DiferencaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double A,B,C,D,Diferenca;
+
+           Console.WriteLine("Informe o 1º número: ");
+           A = double.Parse(Console.ReadLine());
+           
+           Console.WriteLine("Informe o 2º número: ");
+           B = double.Parse(Console.ReadLine());
+           
+           Console.WriteLine("Informe o 3º número: ");
+           C = double.Parse(Console.ReadLine());
+
+           Console.WriteLine("Informe o 4º número: ");
+           D = double.Parse(Console.ReadLine());  
+
+           Diferenca = (A*B - C*D);  
+
+           Console.WriteLine("Diferença = "+Diferenca);   
+        }
+
+    }
+}
