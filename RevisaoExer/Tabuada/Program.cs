16444a2e3a1f36cf4dc7696dbe32aece7044c6db
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-13 22:52:51 -0300

    Exercicio

---
+++ RevisaoExer/Tabuada/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("==================");
+            Console.WriteLine("     TABUADA");
+            Console.WriteLine("==================");
+            Console.WriteLine();
+            int num1 = 1;
+            int num = 0;
+
+            Console.WriteLine("   | INFORME |");
+
+            do{
+            Console.WriteLine();
+            Console.Write("Número: ");
+            num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            if(num > 2 && num < 1000)
+            {
+                for(int i = 0; i < 11; i++ )
+                {
+                    Console.WriteLine($"{num} x {i} = {num*i}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Número Fora dos Padrões");
+                Console.WriteLine("Tente novamente!");
+            }
+
+            }while(num != num1 );
+        }
+    }
+}
