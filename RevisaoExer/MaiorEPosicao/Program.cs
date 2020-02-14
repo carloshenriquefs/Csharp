a674beddc3fdb878a41886e4748f61184d25745d
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-14 00:06:18 -0300

    Exercicio

---
+++ RevisaoExer/MaiorEPosicao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MaiorEPosicao
+{
+    class Program
+    {
+        //Leia 100 valores inteiros. Apresente então o maior valor lido e a posição dentre os 100 valores lidos.
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+           Console.WriteLine("=============================");
+           Console.WriteLine("     | MAIOR | POSICAO |");
+           Console.WriteLine("=============================");
+           Console.WriteLine();
+           
+           
+           int[] num = new int[10];
+
+           for(int i = 1; i < 10; i++)
+           {
+               num[i] = int.Parse(Console.ReadLine());
+               
+           }
+
+           Array.Sort(num);
+           Console.Write("O maior valor é: "+num[9]);
+
+           
+           
+        }
+    }
+}
