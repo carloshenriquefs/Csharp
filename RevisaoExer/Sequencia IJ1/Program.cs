fa8ea670f3b8af9bf77a50f84e56e6a2e2773fbb
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-14 22:52:23 -0300

    Exercicio

---
+++ RevisaoExer/Sequencia IJ1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sequencia_IJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=1, j = 60;
+            for(i = 1,j = 60;j<=60;i=i+3, j-=5)
+            {
+                Console.WriteLine($"I = {i} J= {j}");
+                if(j == 0)
+                    break;
+            }
+          
+           
+        }
+    }
+}
