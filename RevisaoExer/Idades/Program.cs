53ad86956061aa2a66a38778b0be947c446fc638
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-15 14:02:26 -0300

    Exercicios

---
+++ RevisaoExer/Idades/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Idades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Idade = 0;
+            double media = 0;
+            int qtdIdade = 0;
+            Console.WriteLine();
+            Console.WriteLine("============================");
+            Console.WriteLine("        | IDADES |");
+            Console.WriteLine("============================");
+            Console.WriteLine();
+            Console.WriteLine("       [ INFORME ]");
+            Console.WriteLine();
+            Console.WriteLine("A idade: ");
+            do{
+                Idade = double.Parse(Console.ReadLine());
+                media = media + Idade;
+                qtdIdade = qtdIdade + 1;
+                if(Idade < 0)
+                {
+                    break;
+                }
+            }while(Idade > 0);
+
+            Console.WriteLine("==============================");
+            Console.WriteLine("Quantidade de Idades = "+(qtdIdade-1));
+            Console.WriteLine("Media = "+media/(qtdIdade-1));
+            Console.WriteLine("Soma das Idades = "+media);
+            Console.WriteLine("==============================");
+        }
+    }
+}
---
+++ RevisaoExer/SequenciaIJ2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SequenciaIJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, j =7;
+
+            for(i = 1, j = 7; j<=7;i=i+1,j=j-1)
+            {
+                Console.WriteLine($"I = {i} J = {j}");
+            }
+        }
+    }
+}
