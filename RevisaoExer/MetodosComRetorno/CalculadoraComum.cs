596d566cbe59193ce1af5029d0d59e9ffa37f14d
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-19 18:38:22 -0300

    Metodos

---
+++ RevisaoExer/MetodosComRetorno/CalculadoraComum.cs
@@ -0,0 +1,30 @@
+namespace MetodosComRetorno
+{
+    public class CalculadoraComum
+    {
+        public CalculadoraComum()
+        {
+
+        }
+
+        public int Somar(int a, int b)
+        {
+            return a + b; 
+        }
+
+        public int Subtrair(int c, int d)
+        {
+            return c - d;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divisao(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ RevisaoExer/MetodosComRetorno/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MetodosComRetorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculadoraComum = new CalculadoraComum();
+            var resultado = calculadoraComum.Somar(4,3);
+            var resultado1 = calculadoraComum.Subtrair(4,1);
+            var resultado2 = calculadoraComum.Multiplicar(3,2);
+            var resultado3 = calculadoraComum.Divisao(4,2);
+
+            Console.WriteLine(resultado);
+            Console.WriteLine(resultado1);
+            Console.WriteLine(resultado2);
+            Console.WriteLine(resultado3);
+
+        }
+    }
+}
