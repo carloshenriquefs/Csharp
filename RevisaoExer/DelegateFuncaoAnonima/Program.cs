4b4ee9dff18a29fb8939f85aa082eeaeabdc5074
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-25 19:55:24 -0300

    Frase-Invertida-Delegate

---
+++ RevisaoExer/DelegateFuncaoAnonima/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegateFuncaoAnonima
+{
+    class Program
+    {
+        delegate string StringOperacao(string s);
+
+        static void Main(string[] args)
+        {
+            StringOperacao inverter = delegate(string s){
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+            };
+
+            Console.WriteLine("Escreva qualquer coisa: ");
+            string frase = Console.ReadLine();
+            Console.WriteLine(inverter(frase));
+
+        }
+    }
+}
