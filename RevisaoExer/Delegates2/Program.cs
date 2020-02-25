c10eac1310b4a515587dd7663d965f906db52c73
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-25 17:45:02 -0300

    DELEGATES - usabilidade

---
+++ RevisaoExer/Delegates2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegates2
+{
+    class Program
+    {
+        delegate double Soma(double a, double b);
+        delegate void ImprimirSoma(double a, double b);
+        static void Main(string[] args)
+        {
+            Soma op1 = UsandoDelegate.MinhaSoma;
+            Console.WriteLine(op1(2,3.9));
+
+            ImprimirSoma op2 = UsandoDelegate.MenuImprimirSoma;
+            op2(5,8);
+
+            // Func<double, double, double> op3 = UsandoDelegate.MinhaSoma;
+            // Console.WriteLine(op3(2.5,3));
+
+          
+        }
+    }
+}
---
+++ RevisaoExer/Delegates2/UsandoDelegate.cs
@@ -0,0 +1,16 @@
+namespace Delegates2
+{
+    public class UsandoDelegate
+    {
+
+        public static double MinhaSoma(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static void MenuImprimirSoma(double a, double b)
+        {
+            System.Console.WriteLine(a+b);
+        }
+    }
+}
