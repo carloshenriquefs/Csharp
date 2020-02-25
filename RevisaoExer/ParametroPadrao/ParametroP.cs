14e0bac7e90c1a4e83ef957a851b761b637dc9f1
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-02-25 02:16:00 -0300

    ParametroPadrao

---
+++ RevisaoExer/ParametroPadrao/ParametroP.cs
@@ -0,0 +1,12 @@
+namespace ParametroPadrao
+{
+    public class ParametroP
+    {
+        public static int Somar(int a = 1, int b = 1)
+        {
+            return a + b;
+        }
+
+        
+    }
+}
---
+++ RevisaoExer/ParametroPadrao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ParametroPadrao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ParametroP.Somar(10, 23));
+            Console.WriteLine(ParametroP.Somar(50));
+            Console.WriteLine(ParametroP.Somar());
+            Console.WriteLine(ParametroP.Somar( b: 7));
+        }
+    }
+}
