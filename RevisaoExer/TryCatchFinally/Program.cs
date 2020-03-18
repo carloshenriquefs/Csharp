5ea4171be0506f9fbcd2b71e046d1b8146f2b58f
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-18 18:58:59 -0300

    TryCatchFinally

---
+++ RevisaoExer/TryCatchFinally/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+
+            try{
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally{
+                if(fs != null)
+                {
+                    fs.Close();
+                }
+            }
+            
+        }
+    }
+
+  
+}
