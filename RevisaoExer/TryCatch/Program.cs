5da9aa6488b695d23ec72fcf295f2f78fe4e9b89
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-18 18:23:32 -0300

    TryCatch

---
+++ RevisaoExer/TryCatch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int resultado = n1 / n2;
+                Console.WriteLine(resultado);
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error!" + e.Message);
+            }
+        }
+    }
+}
