0e5ac2240571cf6de6a24690e11bf7ec52b78fb3
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-21 18:04:29 -0300

    DuplicarUmArquivo-File_FileInfo

---
+++ TrabalhandoComArquivos/File_FileInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace File_FileInfo
+{
+    class Program
+    {
+        //DUPLICAR UM ARQUIVO: atraves do FILEINFO
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\file.txt";
+            string targetPath = @"c:\temp\file1.txt";
+
+            try
+            {
+                FileInfo file = new FileInfo(sourcePath);
+                file.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(var line in lines)
+                {
+                    Console.WriteLine(line);
+                } 
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
