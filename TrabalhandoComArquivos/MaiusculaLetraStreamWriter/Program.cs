116594c80977ae152caed8ac7f085afc95d47fc1
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-22 20:03:17 -0300

    StreamWriter

---
+++ TrabalhandoComArquivos/MaiusculaLetraStreamWriter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace MaiusculaLetraStreamWriter {
+    class Program {
+        static void Main (string[] args) {
+
+            string sourcePath = @"c:\temp\file.txt";
+            string targetPath = @"c:\temp\file1.txt";
+
+            try {
+                string[] lines = File.ReadAllLines (sourcePath);
+                using (StreamWriter sw = File.AppendText (targetPath)) {
+                    foreach (string line in lines) {
+                        sw.WriteLine (line.ToUpper ());
+                    }
+                }
+            } catch (IOException e) {
+                Console.WriteLine ("Ocorreu um erro");
+                Console.WriteLine (e.Message);
+            }
+
+        }
+    }
+}
