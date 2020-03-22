bc53845022bd2d82a188424bc8916e3efa30c706
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-21 21:45:12 -0300

    FileStream-StreamReader

---
+++ TrabalhandoComArquivos/FileStream_StreamReader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace FileStream_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\file.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            try{
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(e.Message);
+            }
+            finally{
+                if(sr != null) sr.Close();
+                if(fs != null) fs.Close();
+            }
+        }
+    }
+}
