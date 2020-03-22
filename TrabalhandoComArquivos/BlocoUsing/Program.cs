84451cd50d323de9f73b34998caf511d3462d69f
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-21 22:19:33 -0300

    BlocoUsing

---
+++ TrabalhandoComArquivos/BlocoUsing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace BlocoUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file.txt";
+
+            try{
+                using(StreamReader sr = File.OpenText(path)){
+                    while(!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
