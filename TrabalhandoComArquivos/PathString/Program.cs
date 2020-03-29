32a888fd538f5ac1b8a781f5796a8400abaad46c
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-28 22:05:35 -0300

    Path

---
+++ TrabalhandoComArquivos/PathString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace PathString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder\file1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: "+Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: "+Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: "+Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: "+Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithouExtension: "+Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: "+Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: "+ Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: "+Path.GetTempPath());
+        }
+    }
+}
