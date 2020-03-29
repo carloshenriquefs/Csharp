f703bb69a8b667e9129750f9e044f82581481b72
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-03-28 21:40:36 -0300

    Directory

---
+++ TrabalhandoComArquivos/Directory_Teste/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Directory_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myfolder";
+
+            try{
+                IEnumerable<string> folder = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach(string s in folder)
+                {
+                    Console.WriteLine(s);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach(string f in files)
+                {
+                    Console.WriteLine(f);
+                }
+
+                Directory.CreateDirectory(path + "\\newfolder");
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
