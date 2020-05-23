46ab693d251cf7c26083a8869f9967c763437868
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-05-23 01:04:21 -0300

    Teste-solucao

---
+++ RevisaoExer/HelloWord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RevisaoExer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
